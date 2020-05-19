3f7c2549f75d0e0f338e14b1dee5d8b57b3fc3bf
Ruslan <rus.safiullin.01@mail.ru>
2020-05-19 21:14:40 +0300

    Some changes, edit .gitignore

--- src/Homework16/URIDownload.java
+++ src/Homework16/URIDownload.java
@@ -1,16 +1,11 @@
 package Homework16;
 
-import Homework8.Input;
-
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class URIDownload {
     public static void main(String[] args) {
@@ -19,11 +14,10 @@ public class URIDownload {
             URL url = new URL(sc.nextLine());
             URLConnection connection = url.openConnection();
             String type = connection.getContentType();
-            type = type.split("/")[0];
+            type = type.split("/")[1];
             InputStream is = connection.getInputStream();
             String name = "file." + type;
-            new File("./src/Homework16/" + name).createNewFile();
-            File file = new File("./rc/Homework16/" + name);
+            File file = new File("./src/Homework16/" + name);
             Files.copy(is, file.toPath());
         } catch (MalformedURLException e) {
             e.printStackTrace();
